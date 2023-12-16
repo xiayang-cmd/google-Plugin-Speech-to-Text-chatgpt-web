41db0d481ba296cdd116f4337c9dcfdc5bdb42af
夏阳 <62921464+xiayang-cmd@users.noreply.github.com>
2023-12-16 22:43:50 +0800

    Update popup.js

--- CODE_Google/unitVoice2Text/popup.js
+++ CODE_Google/unitVoice2Text/popup.js
@@ -1,12 +1,6 @@
-document.getElementById('insertButton').addEventListener('click', function() {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      let activeTab = tabs[0];
-      chrome.scripting.executeScript({
-        target: {tabId: activeTab.id},
-        files: ['content.js']
-      }, () => {
-        chrome.tabs.sendMessage(activeTab.id, {action: "insertText"});
-      });
-    });
-  });
-  
+document.addEventListener('DOMContentLoaded', function() {
+    var runButton = document.getElementById('runButton');
+    runButton.addEventListener('click', function() {
+        window.location.href = 'myvoice2textapp://';
+    }, false);
+}, false);
