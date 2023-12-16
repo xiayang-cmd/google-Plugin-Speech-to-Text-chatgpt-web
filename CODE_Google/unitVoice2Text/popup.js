046f8ab0c82a16f86308fd2848aeb09c991200bd
夏阳 <62921464+xiayang-cmd@users.noreply.github.com>
2023-12-16 23:26:37 +0800

    Update popup.js

--- CODE_Google/unitVoice2Text/popup.js
+++ CODE_Google/unitVoice2Text/popup.js
@@ -1,6 +1,14 @@
+// document.addEventListener('DOMContentLoaded', function() {
+//     var runButton = document.getElementById('runButton');
+//     runButton.addEventListener('click', function() {
+//         window.location.href = 'myvoice2textapp://';
+//     }, false);
+// }, false);
+
+// 交给background.js处理
 document.addEventListener('DOMContentLoaded', function() {
     var runButton = document.getElementById('runButton');
     runButton.addEventListener('click', function() {
-        window.location.href = 'myvoice2textapp://';
+        chrome.runtime.sendMessage({ action: "runExe" });
     }, false);
 }, false);
